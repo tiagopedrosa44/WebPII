72f8f902a03ae090b479d3d41b2aa5c99a2d1fb7
Tiago <tiagopedrosa44@gmail.com>
2022-12-06 12:43:17 +0000

    key + center in curr loc

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <GMapMap
-      :center="center"
+      :center="getPos()"
       :zoom="18"
       map-type-id="terrain"
       style="width: 500px; height: 300px"
@@ -25,12 +25,27 @@
 markers: [
   {
     position: {
-      lat:  41.36611,
+      lat: 41.36611,
       lng: -8.739542,
     },
   },
 ];
-const center = { lat: 41.36611, lng: -8.739542 };
+
+function getPos(){
+  if (navigator.geolocation.getCurrentPosition((position) => {
+    return {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+  }));
+  else {
+    return {
+      lat: 41.36611,
+      lng: -8.739542,
+    };
+  }
+}
+
 </script>
 
 <style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ app.use(router);
 app.use(vuetify);
 app.use(VueGoogleMaps, {
   load: {
-    key: "",
+    key: "AIzaSyApL3tNB2xZRdCBVU-UGatvU0Dolh5Owo0",
   },
 });
 
