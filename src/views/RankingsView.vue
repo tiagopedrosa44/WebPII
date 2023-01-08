09119697a3dd90da7f0cdd6030c44c7e48cd2efb
Fabio <fabionferreira15@gmail.com>
2023-01-08 13:14:33 +0000

    navbar, perfil,e homepage

--- src/views/RankingsView.vue
+++ src/views/RankingsView.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-        <h1>Leaderboards</h1>
+  <div class="Ranking">
+    <div align="center">
+      <nav>
+        <NavBar />
+      </nav>
     </div>
+    <br>
+    <br>
+    <br>
+
+    <div align="center">
+      <h1>Leaderboard</h1>
+    </div>
+
+    
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import NavBar from "@/components/NavBar.vue";
+export default {
+    components: {
+        NavBar,
+    },
+    data() {
+        return {
+            
+        }
+    },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.Ranking {
+  background: linear-gradient(180deg, #1a9360 0%, #00ad79 47.71%, #40ddae 100%);
+  height: 100vh;
+  width: 100vw;
+}
 </style>
