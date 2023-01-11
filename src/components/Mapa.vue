476c6e8dac399b0a32762fea24d1dccccb8b79d6
Tiago <tiagopedrosa44@gmail.com>
2023-01-11 15:30:52 +0000

    atualizar mapa e ecopontoview

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -1,17 +1,27 @@
 <template>
-  <GoogleMap api-key="AIzaSyAYi2BJ0UcEc3zgu2s6g9UFV-6JHuSkyxE" style="width: 100%; height: 500px" mapTypeId="hybrid"
-    :center="center" :zoom="18">
+  <GoogleMap
+    api-key="AIzaSyAYi2BJ0UcEc3zgu2s6g9UFV-6JHuSkyxE"
+    style="width: 100%; height: 500px"
+    mapTypeId="hybrid"
+    :center="center"
+    :zoom="18"
+  >
     <Marker :options="currentPosMarkerOptions" />
-    <Marker v-for="ecoponto in ecopontos" @click="$router.push('/ecoponto/' + ecoponto.id)" :key="ecoponto.id" :options="{
-      position: {
-        lat: ecoponto.coordenadas.lat,
-        lng: ecoponto.coordenadas.lng,
-      },
-      icon: {
-        url: '/src/assets/imgs/iconeEcoponto.png',
-        scaledSize: { width: 29, height: 40 },
-      },
-    }" />
+    <Marker
+      v-for="ecoponto in ecopontos"
+      @click="focarEcoponto(ecoponto.id)"
+      :key="ecoponto.id"
+      :options="{
+        position: {
+          lat: ecoponto.coordenadas.lat,
+          lng: ecoponto.coordenadas.lng,
+        },
+        icon: {
+          url: '/src/assets/imgs/iconeEcoponto.png',
+          scaledSize: { width: 29, height: 40 },
+        },
+      }"
+    />
   </GoogleMap>
 </template>
 
@@ -22,9 +32,6 @@ import { ecopontoStore } from "../stores/ecopontoStore";
 
 export default defineComponent({
   //recebe o id do ecoponto
-  props: {
-    id: Number,
-  },
   components: { GoogleMap, Marker },
   data() {
     return {
@@ -38,20 +45,35 @@ export default defineComponent({
           url: "/src/assets/imgs/locAtual2.png",
           scaledSize: { width: 29, height: 40 },
         },
+        id: -1,
       },
     };
   },
-  mounted() {
-    console.log(this.id);
+  created() {
+    this.id = this.$route.params.id;
     if (this.id) {
       this.center = {
         lat: this.store.getEcopontoById(this.id).coordenadas.lat,
         lng: this.store.getEcopontoById(this.id).coordenadas.lng,
       };
-      this.currentPosMarkerOptions.position = {
-        lat: this.store.getEcopontoById(this.id).coordenadas.lat,
-        lng: this.store.getEcopontoById(this.id).coordenadas.lng,
-      };
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition((position) => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          this.currentPosMarkerOptions.position = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+        });
+      } else {
+        this.center = { lat: 41.36611, lng: -8.739542 };
+        this.currentPosMarkerOptions.position = {
+          lat: 41.36611,
+          lng: -8.739542,
+        };
+      }
     } else {
       if (navigator.geolocation) {
         navigator.geolocation.watchPosition((position) => {
@@ -66,10 +88,22 @@ export default defineComponent({
         });
       } else {
         this.center = { lat: 41.36611, lng: -8.739542 };
-        this.currentPosMarkerOptions.position = { lat: 41.36611, lng: -8.739542 };
+        this.currentPosMarkerOptions.position = {
+          lat: 41.36611,
+          lng: -8.739542,
+        };
       }
     }
     this.ecopontos = this.store.ecopontos;
+  },
+  methods: {
+    focarEcoponto(id) {
+      this.$router.push('/ecoponto/' + id)
+      this.center = {
+        lat: this.store.getEcopontoById(id).coordenadas.lat,
+        lng: this.store.getEcopontoById(id).coordenadas.lng,
+      };
+    },
   },
 });
 </script>
--- src/views/EcopontoView.vue
+++ src/views/EcopontoView.vue
@@ -1,75 +1,76 @@
 <template>
-    <div class="home">
-        <div align="center">
-            <nav>
-                <NavBar />
-            </nav>
-        </div>
-        <br>
-        <br>
-        <br>
-        <v-container class="grey lighten-5">
-            <v-row no-gutters>
-                <v-col cols="12" lg="4" class="col-ecopontos">
-                    <div class="title" align="center">
-                        <h1>Ecoponto</h1>
-                    </div>
-
-                    <br>
-                    <div id="left">
-                        <img :src="ecoponto.foto">
-                    </div>
-
-                </v-col>
-                <v-col cols="12" lg="8" class="col-mapa" align="center">
-                    <div id="right">
-                        <Mapa :id="ecoponto.id"/>
-                    </div>
-                </v-col>
-            </v-row>
-        </v-container>
+  <div class="home">
+    <div align="center">
+      <nav>
+        <NavBar />
+      </nav>
     </div>
+    <br />
+    <br />
+    <br />
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col cols="12" lg="4" class="col-ecopontos">
+          <div class="title" align="center">
+            <h1>Ecoponto</h1>
+          </div>
+
+          <br />
+          <div id="left">
+            <img :src="ecoponto.foto" />
+          </div>
+        </v-col>
+        <v-col cols="12" lg="8" class="col-mapa" align="center">
+          <div id="right">
+            <Mapa />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
-  
+
 <script>
 import NavBar from "@/components/NavBar.vue";
 import Mapa from "@/components/Mapa.vue";
 import { ecopontoStore } from "../stores/ecopontoStore.js";
 
 export default {
-    components: {
-        NavBar,
-        Mapa,
-    },
-    data() {
-        return {
-            id: this.$route.params.id,
-            ecopontoStore: ecopontoStore(),
-            ecoponto: [],
-        };
-    },
-    created() {
-        this.ecoponto = this.ecopontoStore.getEcopontoById(this.id);
-        console.log(this.ecoponto);
-    },
+  components: {
+    NavBar,
+    Mapa,
+  },
+  data() {
+    return {
+      id: this.$route.params.id,
+      ecopontoStore: ecopontoStore(),
+      ecoponto: [],
+    };
+  },
+  created() {
+    this.ecoponto = this.ecopontoStore.getEcopontoById(this.id);
+    console.log(this.ecoponto);
+    this.$watch(
+      () => this.$route.params,
+      (toParams, previousParams) => {
+        if (toParams.id !== previousParams.id) {
+          this.id = toParams.id;
+          this.ecoponto = this.ecopontoStore.getEcopontoById(this.id);
+        }
+      }
+    );
+  },
 };
 </script>
-  
 
 <style scoped>
+/**id left aligned to the left and id right aligned to the right */
 #left {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 100%;
-    width: 100%;
+  float: left;
+  width: 50%;
 }
-
 #right {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    height: 100%;
-    width: 100%;
+  float: right;
+  width: 50%;
 }
 </style>
